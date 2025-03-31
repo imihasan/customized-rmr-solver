e7c978b9f192740c05b7a00c9aacb4b20e57a109
Ibrahim Hasan <122603573+imihasan@users.noreply.github.com>
2025-03-31 21:39:29 +0200

    Delete Code/Data Processing directory

--- Code/Data Processing/EMGnorm.m
+++
@@ -1,7 +0,0 @@
-function [EMGvalue,percMot]=EMGnorm(data,startind,endind,indmax)
-up = data(startind:indmax);
-perc01 = linspace(0,100,length(up));
-down = data(indmax:endind);
-percd01 = linspace(100,200,length(down));
-percMot = [perc01 percd01];
-EMGvalue = [up' down'];
--- Code/Data Processing/meanstd.m
+++
@@ -1,5 +0,0 @@
-function [average,stdup,stdlow] = meanstd(data01,data02,data03)
-comp = [data01;data02;data03];
-average = mean(comp);stdev = std(comp);
-stdup = average+stdev;
-stdlow = average-stdev;
